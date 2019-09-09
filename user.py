3e477df28a6288403cb437cf28176105aaab1665
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-09 12:36:34 +0200

    added user.py

--- user.py
+++ user.py
@@ -7,10 +7,10 @@ class User:
     users_array = []
 
     def __init__(self, first_name, last_name, phone_number, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.email = email
+        # self.first_name = first_name
+        # self.last_name = last_name
+        # self.phone_number = phone_number
+        # self.email = email
 
     def save_user_details(self):
         """
